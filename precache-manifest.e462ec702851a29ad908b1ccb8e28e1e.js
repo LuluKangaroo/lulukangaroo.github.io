f9e96452ed39b865a7a5d42f32a9933d97eb8f73
Lulu Liu <lululiu85@gmail.com>
2019-10-24 20:20:11 -0600

    Updates

---
+++ precache-manifest.e462ec702851a29ad908b1ccb8e28e1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c70500ccdd62ed19f989",
+    "url": "/static/js/main.36c7faaa.chunk.js"
+  },
+  {
+    "revision": "78ea14cf809a8c575099",
+    "url": "/static/js/2.3174e710.chunk.js"
+  },
+  {
+    "revision": "c70500ccdd62ed19f989",
+    "url": "/static/css/main.b9da0e65.chunk.css"
+  },
+  {
+    "revision": "f61ea26f0eed3e9863e7a3cbd09744aa",
+    "url": "/index.html"
+  }
+];
