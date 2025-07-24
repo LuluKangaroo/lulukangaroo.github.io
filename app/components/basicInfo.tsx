0cd93ceecfbe5283e55d7a7a86ea2328b4d4f3cb
Lulu Kangaroo <lululiu85@gmail.com>
2025-07-24 15:35:25 -0600

    json template for experiences and components for resume section

---
+++ app/components/basicInfo.tsx
@@ -0,0 +1,10 @@
+export function BasicInfo() {
+    return (
+        <div>
+            <h1>First Last</h1>
+            <p>email</p>
+            <p>location</p>
+            <p>education</p>
+        </div>
+    );
+}
--- app/components/resume.tsx
+++ app/components/resume.tsx
@@ -1,7 +1,10 @@
+import { BasicInfo } from "./basicInfo";
+
 export function Resume() {
     return (
         <div>
             <h1>hello you're in resume</h1>
+            <BasicInfo />
         </div>
     );
 }
