2d56acc652ca79fbc2013233e9efc3d8a3ac122f
Lulu Kangaroo <lululiu85@gmail.com>
2025-08-06 13:43:36 -0600

    light/dark mode toggle button

--- app/components/toggleMode.tsx
+++ app/components/toggleMode.tsx
@@ -1,7 +1,23 @@
+import { useState, useEffect } from "react";
+import { LightBulbIcon } from '@heroicons/react/24/outline';
+
 export function ToggleMode() {
+    const savedDark = localStorage.getItem('darkMode') === 'true';
+    const [darkMode, setDarkMode] = useState(savedDark);
+
+    useEffect(() => {
+        if (darkMode) {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('darkMode', 'true');
+        } else {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('darkMode', 'false');
+        }
+    }, [darkMode]);
+
     return (
-        <p>
-            light/dark
-        </p>
+        <button onClick={() => setDarkMode(!darkMode)}>
+            <LightBulbIcon className="size-8"/>
+        </button>
     )
 }
