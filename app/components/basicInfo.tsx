e77f680edef0240135abd44839827c12a0199f09
Lulu Kangaroo <lululiu85@gmail.com>
2025-08-06 14:56:05 -0600

    display font and contact info restructure

--- app/components/basicInfo.tsx
+++ app/components/basicInfo.tsx
@@ -1,16 +1,28 @@
 import { ToggleMode } from "./toggleMode";
+import { SiGithub, SiLinkedin, SiLeetcode, SiCodecademy } from "react-icons/si";
+import { FaBomb } from "react-icons/fa";
 
 export function BasicInfo() {
     return (
-        <div className="sticky top-0 -mb-50
-                        box-border h-30 w-vw
-                        bg-white dark:bg-black">
-            <h1>First Last</h1>
-            <div>
+        <div className="sticky top-0 -mb-20 z-10
+                        box-border h-20 w-dvw
+                        bg-white/90 dark:bg-black/90
+                        flex flex-col justify-center items-center">
+            <h1>Lulu</h1>
+            <div className="hidden">
                 <p>email</p>
                 <p>location</p>
                 <p>education</p>
             </div>
+            <ul className="box-border
+                           w-screen
+                           flex flex-row justify-center">
+                <li><SiLinkedin /></li>
+                <li><SiGithub /></li>
+                <li><SiLeetcode /></li>
+                <li><SiCodecademy /></li>
+                <li><FaBomb /></li>
+            </ul>
             <ToggleMode />
         </div>
     );
--- app/components/splash.tsx
+++ app/components/splash.tsx
@@ -1,6 +1,4 @@
 import { BasicInfo } from "../components/basicInfo";
-import { SiGithub, SiLinkedin, SiLeetcode, SiCodecademy } from "react-icons/si";
-import { FaBomb } from "react-icons/fa";
 
 export function Splash() {
     return (
@@ -12,21 +10,11 @@ export function Splash() {
                         flex flex-col justify-center items-center gap-4
                         text-black dark:text-white
                         snap-always snap-center">   
-            <BasicInfo />
-            <h1 className="text-9xl">
-                Lu Liu
+            <h1 className="font-display text-[30vh] leading-none">
+                 
             </h1>
-            <sub>some sort of description here</sub>
-            <ul className="box-border
-                           w-screen
-                           flex flex-row justify-center">
-                <li><SiGithub /></li>
-                <li><SiLinkedin /></li>
-                <li><SiLeetcode /></li>
-                <li><SiCodecademy /></li>
-                <li><FaBomb /></li>
-            </ul>
-            <h2>resume (down/scroll)</h2>
+            <BasicInfo />            
+            <h2 className="pt-20">resume (down/scroll)</h2>
         </div>
     );
 }
