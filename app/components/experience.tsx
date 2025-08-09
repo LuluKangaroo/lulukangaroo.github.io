a79f6cd37bfc681c4ecc843a5aff34c3e9ad2926
Lulu Kangaroo <lululiu85@gmail.com>
2025-08-09 12:42:39 -0600

    updating resume content to read from experiences json

--- app/components/experience.tsx
+++ app/components/experience.tsx
@@ -1,21 +1,26 @@
-export function Experience() {
+export function Experience({ role, company, duration, achievements }) {
+    const headerStyles = "font-header lowercase text-2xl mt-5 text-black/20 dark:text-white/30";
+
+    let formatAchievements = [];
+    for (let line = 0; line < achievements.length; line++) {
+        formatAchievements.push(<li>{achievements[line]}</li>);
+    }
+    let startDate = duration[0];
+    let endDate = duration[1];
     return (
         <div>
-            <h1>role</h1>
-            <sub className="box-border flex flex-row justify-between">
-                <span>company</span>
-                <span>duration</span>
-            </sub>
+            <div className="box-border flex flex-row justify-between border-b-2 border-black/20 dark:border-white/30 mt-10">
+                <h1 className="font-bold">{role}</h1>
+                <h1 className={headerStyles}>{company}</h1>
+            </div>
+            <span>{startDate} - {endDate}</span>
             <ul>
                 <li>tools</li>
                 <li>languages</li>
                 <li>softwares</li>
             </ul>
             <ul>
-                <li>achievement 1</li>
-                <li>achievement 2</li>
-                <li>achievement 3</li>
-                <li>achievement 4</li>
+                {formatAchievements}
             </ul>
         </div>
     )
--- app/components/resume.tsx
+++ app/components/resume.tsx
@@ -1,18 +1,20 @@
 import { Experience } from "./experience";
 import { MdOutlineMail, MdOutlineHome } from "react-icons/md";
 import { PiGraduationCapFill } from "react-icons/pi";
+import expData from "../data/experiences.json";
 
 export function Resume() {
     const headerStyles = "font-header lowercase text-2xl mt-5 text-black/20 dark:text-white/30";
+    console.log(expData);
 
     return (
         <div className="box-border
-                        h-dvh pt-20
+                        h-dvh p-10
                         bg-white dark:bg-black
                         text-black dark:text-white
                         snap-always snap-center
                         flex flex-row">
-            <div id="sidebar" className="box-border w-[35vw] pl-20 pr-10 border-r-2 border-black/20">
+            <div id="sidebar" className="box-border w-[35vw] pr-10 border-r-2 border-black/20 dark:border-white/30">
                 <h1 className={headerStyles}>Overview</h1>
                 <ul>
                     <li>maybe some sort of short summary speil right here that's the overview of what i know</li>
@@ -21,17 +23,21 @@ export function Resume() {
                 </ul>
                 <h1 className={headerStyles}>Education</h1>
                 <ul>
-                    <li>Bachelors of Science in Computer Science</li>
+                    <li>B.S. in Computer Science</li>
                     <li>University of Colorado, Boulder</li>
                     <li><PiGraduationCapFill className="inline"/> May 2020</li>
                     <li>GPA 3.2</li>
                 </ul>
             </div>
             <div id="resumeContent" className="box-border w-full overflow-y-auto pl-10">
-                <Experience />
-                <Experience />
-                <Experience />
-                <Experience />
+                {expData.map(item => (
+                    <Experience
+                        role={item.role}
+                        company={item.company}
+                        duration={item.duration}
+                        achievements={item.achievements}
+                    />
+                ))}
             </div>
         </div>
     );
