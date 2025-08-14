11c27c7deaeaea25e63d86ced95a5ded310f16bb
Lulu Kangaroo <lululiu85@gmail.com>
2025-08-14 12:10:47 -0600

    linting config

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,28 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import react from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  react.configs.flat.recommended,
+  {
+    plugins: {
+      react,
+      js
+    },
+    rules: {
+        // suppress errors for missing 'import React' in files
+      "react/react-in-jsx-scope": "off",
+        // allow jsx syntax in js files (for next.js project)
+      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx", ".tsx"] }], //should add ".ts" if typescript project
+    },
+    files: [
+      "**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"
+    ],
+    languageOptions: { 
+      globals: globals.browser 
+    },
+  },
 ]);
