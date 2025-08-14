a13b8ee04fe3b5efae4fa5fcbf5c0d4ae2119260
Lulu Kangaroo <lululiu85@gmail.com>
2025-08-14 12:12:29 -0600

    linting and props types

--- app/components/experience.tsx
+++ app/components/experience.tsx
@@ -1,12 +1,19 @@
-export function Experience({ role, company, duration, achievements }) {
+type ExperienceProps = {
+    role: string
+    company: string
+    duration: Array<string>
+    achievements: Array<string>
+}
+
+export function Experience({ role, company, duration, achievements }: ExperienceProps) {
     const headerStyles = "font-header lowercase text-2xl mt-5 text-black/20 dark:text-white/30";
 
-    let formatAchievements = [];
+    const formatAchievements = [];
     for (let line = 0; line < achievements.length; line++) {
         formatAchievements.push(<li>{achievements[line]}</li>);
     }
-    let startDate = duration[0];
-    let endDate = duration[1];
+    const startDate = duration[0];
+    const endDate = duration[1];
     return (
         <div>
             <div className="box-border flex flex-row justify-between border-b-2 border-black/20 dark:border-white/30 mt-10">
